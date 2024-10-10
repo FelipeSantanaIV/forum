1729868e68cbc6a510c6a61704afd12e2d07f2c8
Felipe Sant Ana <felipe.santana1995@hotmail.com>
2024-10-10 13:46:06 -0300

    Que podemos usar tokens para realizar autenticação e autorização dos nossos usuários; Que por meio do site jwt.io conseguimos debugar os nossos tokens; Que esses tokens são auto-contidos, ou seja, tudo que é necessário para autenticação e autorização deve estar nele; Que podemos usar o token utilizando a dependência jjwt, juntamente com o spring-security; Que para conseguir pegar o token a cada requisição, precisamos criar filtros na nossa aplicação e fazer a validação do token;

---
+++ src/main/kotlin/br/com/felipe/forum/config/JWTUtil.kt
@@ -0,0 +1,63 @@
+package br.com.felipe.forum.config
+
+import br.com.felipe.forum.service.UsuarioService
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import java.util.*
+import javax.crypto.SecretKey
+
+@Component
+class JWTUtil(
+    private val userService: UsuarioService
+) {
+    @Value("\${jwt.secret}")
+    lateinit var secret: String
+
+    private lateinit var key: SecretKey
+
+    @PostConstruct
+    fun init() {
+        key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
+    }
+
+    private val expiration: Long = 60000
+
+    fun generateToken(username: String, authorities: MutableCollection<out GrantedAuthority>): String {
+        return Jwts
+            .builder()
+            .subject(username)
+            .claim("role", authorities)
+            .expiration(Date(System.currentTimeMillis() + expiration))
+            .signWith(key)
+            .compact()
+    }
+
+    fun isValid(jwt: String?): Boolean {
+        return try {
+            Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(jwt)
+            true
+        } catch (e: IllegalArgumentException) {
+            false
+        }
+    }
+
+    fun getAuthentication(jwt: String?): UsernamePasswordAuthenticationToken {
+        val username = Jwts.parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(jwt)
+            .payload
+            .subject
+        val user = userService.loadUserByUsername(username.toString())
+        return UsernamePasswordAuthenticationToken(username, null, user.authorities)
+    }
+}
--- src/main/kotlin/br/com/felipe/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/felipe/forum/config/SecurityConfiguration.kt
@@ -1,10 +1,13 @@
 package br.com.felipe.forum.config
 
+import br.com.felipe.forum.security.JWTAuthenticationFilter
+import br.com.felipe.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -12,10 +15,16 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration (
+    private val configuration: AuthenticationConfiguration,
+    private val userDetailsService: UserDetailsService,
+    private val jwtUtil: JWTUtil
+){
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -23,15 +32,22 @@ class SecurityConfiguration {
             .csrf { it.disable() }
             .authorizeHttpRequests {
                 it
-                    .requestMatchers(HttpMethod.GET, "/topics").hasAuthority("LEITURA_E_ESCRITA")
+                    .requestMatchers(HttpMethod.GET, "/topicos").hasAuthority("LEITURA_E_ESCRITA")
+                    .requestMatchers(HttpMethod.POST, "/login").permitAll()
                     .anyRequest().authenticated()
             }
+            .addFilterBefore(JWTLoginFilter(authenticationManager = configuration.authenticationManager, jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+            .addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            }.httpBasic(Customizer.withDefaults()).build()
+            }.build()
     }
 
     @Bean
     fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    fun configure(auth: AuthenticationManagerBuilder?) {
+        auth?.userDetailsService(userDetailsService)?.passwordEncoder(encoder())
+    }
 
 }
