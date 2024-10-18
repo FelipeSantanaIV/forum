c34faabba0770dfe249faeccb36ed6dec1fd7b86
Felipe Sant Ana <felipe.santana1995@hotmail.com>
2024-10-18 11:35:35 -0300

    Testes de API são usados para testar as APIs da aplicação; Podemos usar uma biblioteca chamada MockMVC para fazer este tipo de teste; Os testes de API fazem uma requisição real para a API, então se ela depender de autenticação, o teste precisará ter uma lógica de autorização; Com testes de APIs podemos validar os parâmetros da requisição, o body retornado, o código HTTP, dentre outros detalhes.

--- src/test/kotlin/br/com/felipe/forum/model/UsuarioTest.kt
+++ src/test/kotlin/br/com/felipe/forum/model/UsuarioTest.kt
@@ -2,5 +2,5 @@ package br.com.felipe.forum.model
 
 object UsuarioTest {
     fun build() = Usuario(id = 1, nome = "Joao", email = "jvc.martins", password = "123")
-    fun buildToToken() = Usuario(nome = "Ana da Silva", email = "ana@email.com", password = "123456")
+    fun buildToToken() = Usuario(1,nome = "Ana da Silva", email = "ana@email.com", password = "123456")
 }
